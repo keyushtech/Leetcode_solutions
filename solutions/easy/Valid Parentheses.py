40134b95502e64c1b8060ebb8962dbe163e3e6a4
Keyush Sai Velagapudi <velagapudik@iitbhilai.ac.in>
2026-08-01 19:35:08 +0530

    Add solution for Valid Parentheses

---
+++ solutions/easy/Valid Parentheses.py
@@ -0,0 +1,23 @@
+// Title: Valid Parentheses
+            // Difficulty: Easy
+            // Language: Python
+            // Link: https://leetcode.com/problems/valid-parentheses/
+
+        for char in s:
+            if char in mapping:
+                # Pop top element if stack is 
+                non-empty; else assign dummy value
+
+        mapping = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+            return False
+        if len(s) % 2 != 0:
+        can never be balanced
+        # Quick edge case check: odd length strings 
+        """
+        :rtype: bool
+        :type s: str
+        """
+    def isValid(self, s):
+class Solution(object):
