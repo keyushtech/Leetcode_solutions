078a564c124728f63a4b64efdf642d5624282214
Keyush Sai Velagapudi <velagapudik@iitbhilai.ac.in>
2026-09-06 23:45:48 +0530

    Add solution for Find Missing and Repeated Values

---
+++ solutions/easy/Find Missing and Repeated Values.cpp
@@ -0,0 +1,21 @@
+// Title: Find Missing and Repeated Values
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/find-missing-and-repeated-values/
+
+        }
+        
+        for (int i =1;i<=(grid.size()*grid.size());i++){
+            if (freq[i]==2){
+            }
+                dupe = i;
+            }
+                miss = i;
+        int dupe=0;
+        int miss=0;
+            
+            if (freq[i]==0){
+        }
+        return {dupe, miss};
+    }
+};
